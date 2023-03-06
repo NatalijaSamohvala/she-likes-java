24d077336163bd0e3cd54c7be355e8be3cf37313
Natalija Samohvala <natalija.samohvala@gmail.com>
2023-03-06 18:57:26 +0200

    Exercise 7 - in class

--- src/Main.java
+++ src/Main.java
@@ -65,6 +65,19 @@ public class Main {
         System.out.println("QUOTIENT");
         System.out.println(trueGala/trueHoneyCrisp);
 
+        System.out.println("Exercise 7");
+        int sum = 5 + 8;
+        System.out.println("Sum of 5 and 8 is = " + sum);
+
+        int difference = 11 - 4;
+        System.out.println("Difference of 11 and 4 is: " + difference);
+
+        int product = 5 * 8;
+        System.out.println("Product of 5 and 8 is: " + product);
+
+        double quotient = 5.0/8.5;
+        System.out.println("Quotient of 5.0 and 8.5 is: " + quotient);
+
 
 
 
