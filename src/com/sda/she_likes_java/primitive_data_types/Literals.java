64cd19bd2c15fe222596e7b8e4d596f4db874a7a
Natalija Samohvala <natalija.samohvala@gmail.com>
2023-03-19 10:21:52 +0200

    literals explained

---
+++ src/com/sda/she_likes_java/primitive_data_types/Literals.java
@@ -0,0 +1,22 @@
+package com.sda.she_likes_java.primitive_data_types;
+
+public class Literals {
+    public static void main(String[] args) {
+        // literal is direct value
+        // String literal is e.g. "name"
+
+        String name = "Natalija";
+
+        // integer literal
+        int age = 40; // 40 is type int
+
+        // long literal
+        long anotherAge = 30L;
+
+        // double literal
+        double doubleNumber = 3.1;
+
+         // float literal
+        float floatNumber = 3.1F;
+    }
+}
