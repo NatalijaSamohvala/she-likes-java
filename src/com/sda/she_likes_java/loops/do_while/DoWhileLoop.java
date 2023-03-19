76c49904b0b713e3c91d206ae0c6e8432643303f
Natalija Samohvala <natalija.samohvala@gmail.com>
2023-03-19 13:06:21 +0200

    Do While first example

---
+++ src/com/sda/she_likes_java/loops/do_while/DoWhileLoop.java
@@ -0,0 +1,13 @@
+package com.sda.she_likes_java.loops.do_while;
+
+import java.sql.SQLOutput;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        boolean isAdult = true;
+        do {
+            System.out.println("Can You see this line???");
+        } while (isAdult);
+        }
+    }
+
