df25fd15908c1a4d18e150da6c600492e39f8ae5
Natalija Samohvala <natalija.samohvala@gmail.com>
2023-03-20 21:48:37 +0200

    for loop example with grades

---
+++ src/com/sda/she_likes_java/arrays/EnglishSchoolGrades.java
@@ -0,0 +1,19 @@
+package com.sda.she_likes_java.arrays;
+
+import java.util.Random;
+
+public class EnglishSchoolGrades {
+    public static void main(String[] args) {
+        int[] myGradesOnEnglishClass = new int[10000];
+
+        Random gradeGenerator = new Random (7);
+// initializing grades with some random scores
+        for(int index = 0; index < myGradesOnEnglishClass.length; index++) {
+            myGradesOnEnglishClass[index] = 1+ gradeGenerator.nextInt(6);
+        }
+
+        for(int index = 0; index < myGradesOnEnglishClass.length; index++) {
+            System.out.println("my grade on index number: " + index + " is " + myGradesOnEnglishClass[index]);
+        }
+    }
+}
