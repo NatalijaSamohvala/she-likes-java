c37542c2c76b7c67d9fd5fa05e3945fcc462d710
Natalija Samohvala <natalija.samohvala@gmail.com>
2023-04-03 20:57:48 +0300

    Static example with WashMachine class

---
+++ src/com/sda/she_likes_java/static_modifier/WashMachine.java
@@ -0,0 +1,42 @@
+package com.sda.she_likes_java.static_modifier;
+public class WashMachine {
+    private String brand;
+    private String model;
+    private int serialNumber;
+    private static int numberOfCreatedMachines;
+
+    public WashMachine(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+        numberOfCreatedMachines++;
+        serialNumber++;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public static void setNumberOfCreatedMachines(int numberOfCreatedMachines) {
+        WashMachine.numberOfCreatedMachines = numberOfCreatedMachines;
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+    public static int getNumberOfCreatedMachines() {
+        return numberOfCreatedMachines;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public String getModel() {
+        return model;
+    }
+    @Override
+    public String toString() {
+        return "WashMachine{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+}
